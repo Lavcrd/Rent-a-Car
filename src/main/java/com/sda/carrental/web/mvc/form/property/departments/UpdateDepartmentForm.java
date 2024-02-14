fb198e4dce6a036aff3c09b43594f1c6f4ff3381
Lavcrd <zlatkov.panayotov@gmail.com>
2024-02-14 20:50:24 +0100

    update/added modals for mg-dpt viewPage update/added constructors to details and contacts dpt forms

--- src/main/java/com/sda/carrental/web/mvc/form/property/departments/UpdateDepartmentForm.java
+++ src/main/java/com/sda/carrental/web/mvc/form/property/departments/UpdateDepartmentForm.java
@@ -1,5 +1,6 @@
 package com.sda.carrental.web.mvc.form.property.departments;
 
+import com.sda.carrental.model.property.Department;
 import com.sda.carrental.web.mvc.form.common.ConfirmationForm;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,4 +24,11 @@ public class UpdateDepartmentForm extends ConfirmationForm {
     @NotBlank(message = "Failure: Postcode field cannot be empty!")
     @Length(min = 1, max = 15, message = "Failure: Please enter a valid postcode.")
     private String postcode;
+
+
+    public UpdateDepartmentForm(Department department) {
+        this.city = department.getCity();
+        this.address = department.getAddress();
+        this.postcode = department.getPostcode();
+    }
 }
